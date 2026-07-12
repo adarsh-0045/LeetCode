069b1ccf541cb4f615a7f77d3444b2f92acbe7ba
ay1874004-svg <ay1874004@gmail.com>
2026-07-12 15:41:08 +0530

    Time: 0 ms (100%), Space: 35.6 MB (79.69%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left=0 , sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum=sum+nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            int right=sum-nums[i]-left;
+            if(left==right)
+            return i;
+            left=left+nums[i];
+        }
+        return -1;
+    }
+};
