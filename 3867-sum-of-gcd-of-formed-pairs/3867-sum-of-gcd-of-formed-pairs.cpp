e3f9fa39833309aa707481da2e74982f6d27770b
ay1874004-svg <ay1874004@gmail.com>
2026-07-23 17:58:18 +0530

    Time: 77 ms (39.81%), Space: 155.6 MB (59.84%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+        int prefixMax = 0;
+        vector<int> prefixGcd(n);
+        for (int i = 0; i < n; ++i) {
+            prefixMax = max(prefixMax, nums[i]);
+            prefixGcd[i] = gcd(nums[i], prefixMax);
+        }
+        sort(prefixGcd.begin(), prefixGcd.end());
+        long long ans = 0;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            ans += gcd(prefixGcd[left], prefixGcd[right]);
+            left++;
+            right--;
+        }
+        return ans;
+    }
+};
