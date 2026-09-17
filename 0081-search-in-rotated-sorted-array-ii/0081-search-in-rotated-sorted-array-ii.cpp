0b782f8d5c1240d3fe6eab3708279497bf1d8621
Adarsh Yadav <ay1874004@gmail.com>
2026-09-17 19:26:25 +0530

    Time: 0 ms (100%), Space: 17.9 MB (80.52%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,73 +1,25 @@
-// class Solution {
-// public:
-//     bool search(vector<int>& nums, int target) {
-//         int left=0;
-//         int right=nums.size()-1;
-//         while(left<=right){
-//             int mid=left+(right-left)/2;
-//             int midVal=nums[mid];
-//             if(midVal==target) return true;
-//             if(nums[left]==midVal && midVal==target){
-//                 left++;
-//                 right--;
-//                 continue;
-//             }
-//             else if(nums[left]<=midVal){
-//                 if(target>=nums[left] && target<midVal) right=mid-1;
-//                 else left=mid+1;
-//             }
-//             else{
-//                 if(target<=nums[right] && target>midVal) left=mid+1;
-//                 else right=mid-1;
-//             }
-//         }
-//         return false;
-//     }
-// };
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
-                return true;
-
-            // Cannot determine which half is sorted
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return true;
+            if(nums[left]==nums[mid] && nums[mid]==nums[right]){
                 left++;
                 right--;
                 continue;
             }
-
-            // Left half is sorted
-            if (nums[left] <= nums[mid]) {
-
-                // Target lies in left sorted half
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                }
-                else {
-                    left = mid + 1;
-                }
+            else if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<nums[mid]) right=mid-1;
+                else left=mid+1;
             }
-
-            // Right half is sorted
-            else {
-
-                // Target lies in right sorted half
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
+            else{
+                if(target<=nums[right] && target>nums[mid]) left=mid+1;
+                else right=mid-1;
             }
         }
-
         return false;
     }
 };
